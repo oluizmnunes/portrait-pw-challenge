1d1bdf9d6d1e2446b767bcfc6ffc26559363ee26
oluizmnunes <oluizmnunes@gmail.com>
2025-10-29 03:34:19 -0300

    add first login test with new structure

--- tests/authentication.spec.ts
+++ tests/authentication.spec.ts
@@ -4,6 +4,21 @@ import { PageManager } from '../page-objects/pageManager'
 import { NavigationPage } from '../page-objects/navigationPage'
 
 test.describe('valid login scenarios', () => {
-    test('should login successfully', async ({ page }) => {
+  test('should login successfully', async ({ page }) => {
+      const pm = new PageManager(page);
+      const navigateTo = new NavigationPage(page);
+
+      // Act
+      await pm.navigateTo().loginPage();
+      await expect(pm.onLoginPage().emailInput).toBeVisible();
+      await expect(pm.onLoginPage().passwordInput).toBeVisible();
+      await pm.onLoginPage().togglePasswordVisibility(); // facilitate 
+      await expect(pm.onLoginPage().passwordInput).toHaveAttribute('type', 'text');
+      await pm.onLoginPage().inputEmailAndPassword('admin@test.com', 'Admin123!'); // TODO: eliminate hardcoding asap
+      await pm.onLoginPage().loginButton.click()
+
+      // Assert
+      await page.waitForURL('/dashboard')
+      await expect(pm.onDashboardPage().dashboardTitle).toContainText('Dashboard')
   })
 })
